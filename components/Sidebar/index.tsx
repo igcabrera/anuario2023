7b9744efc71b143566345766caef5b220322ba9a
igcabrera.dev <igcabrera.dev@gmail.com>
2024-03-25 12:51:09 -0300

    fix2

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -57,7 +57,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <aside
       ref={sidebar}
-      className={`absolute left-0 top-0 z-7 flex h-screen w-[330px] flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
+      className={`absolute left-0 top-0 z-99 flex h-screen w-[330px] flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
         sidebarOpen ? "translate-x-0" : "-translate-x-full"
       }`}
     >
